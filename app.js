919787d53c1d39b33b587d8613a03f076c12a395
AliShahpurwala <ali.murtaza.am@gmail.com>
2020-10-25 09:05:39 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	  	var codeEditorElement = document.getElementById("ed_code");
+  		var editor = CodeMirror.fromTextArea(codeEditorElement, {
+  		lineNumbers: true,
+  		theme: "ambiance"
+  	});
+  	editor.setSize(650,400);
+
+    var runButton = document.getElementById("run");
+    runButton.addEventListener("click", function(e){
+        console.log(editor.getValue());
+        var startPointProvided = document.getElementById("startPointProvided").checked;
+        var functionName = $("#codeStartPoint").val();
+        submittedCode = editor.getValue();
+        $.ajax({
+          method: 'POST',
+          url: 'https://qnjsnfaz9l.execute-api.us-east-1.amazonaws.com/v1/codeevaluatorgateway',
+          dataType: 'json',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            'code': submittedCode,
+            'startPointProvided': startPointProvided,
+            'functionName':  functionName
+          })
+        })
+        .done((res) => { 
+          console.log(res);
+          document.getElementById("results").textContent = res["result"];
+        });
+    }, false
+    );
+
+});
